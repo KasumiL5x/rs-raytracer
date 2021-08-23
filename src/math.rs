0f8b4692b0c58cba52ce91e3844a217e497875b5
KasumiL5x <KasumiL5x@gmail.com>
2021-08-23 23:46:53 +0100

    Simplified sphere intersection.

--- src/math.rs
+++ src/math.rs
@@ -171,14 +171,14 @@ impl Ray {
 // --------------------------------------------------
 pub fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> f32 {
   let oc = ray.origin - (*center);
-  let a = ray.direction.dot(&ray.direction);
-  let b = 2.0 * oc.dot(&ray.direction);
-  let c = oc.dot(&oc) - (radius * radius);
-  let discriminant = (b * b) - (4.0 * a * c);
+  let a = ray.direction.sqr_length();
+  let half_b = oc.dot(&ray.direction);
+  let c = oc.sqr_length() - (radius * radius);
+  let discriminant = (half_b * half_b) - (a * c);
 
   if discriminant < 0.0 {
     return -1.0
   } else {
-    return (-b - discriminant.sqrt()) / (2.0 * a)
+    return (-half_b - discriminant.sqrt()) / a
   }
 }
