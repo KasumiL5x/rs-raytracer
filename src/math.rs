e9b35e827bcfc99f88e8399cf124b0165dac1bfc
KasumiL5x <KasumiL5x@gmail.com>
2021-08-23 09:12:11 +0100

    Added basic sky although it appears inverted? Don't have time to check it out now, unfortunatly.

--- src/math.rs
+++ src/math.rs
@@ -153,6 +153,13 @@ pub struct Ray {
 }
 
 impl Ray {
+  pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+    Ray {
+      origin: origin,
+      direction: direction
+    }
+  }
+
   pub fn at(&self, t: f32) -> Vec3 {
     self.origin + self.direction * t
   }
--- src/raytracer.rs
+++ src/raytracer.rs
@@ -5,6 +5,8 @@ use std::io::prelude::*;
 use std::io::BufWriter;
 use std::fs::File;
 
+use crate::math::*;
+
 pub const IMAGE_WIDTH: u32 = 1280;
 pub const IMAGE_HEIGHT: u32 = 720;
 
@@ -74,7 +76,20 @@ impl RSRaytracer {
         println!("Starting ray tracer...");
         let start_time = std::time::Instant::now();
 
-        // TODO: Ray tracing code. For now it's random noise.
+        // Image details.
+        let aspect_ratio = (IMAGE_WIDTH as f32) / (IMAGE_HEIGHT as f32);
+        // Camera details.
+        let viewport_height = 2.0; // Why 2?
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+        //
+        let origin = Vec3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner = origin - (horizontal * 0.5) - (vertical * 0.5) - Vec3::new(0.0, 0.0, focal_length);
+
+
+        // TODO: Ray tracing code.
         let mut rng = rand::thread_rng();
         let pitch = (IMAGE_WIDTH * 3) as usize;
         for y in 0..(IMAGE_HEIGHT as usize) {
@@ -82,10 +97,19 @@ impl RSRaytracer {
             for x in 0..(IMAGE_WIDTH as usize) {
                 let offset = y * pitch + x * 3;
 
-                let rand_val: u32 = rng.gen::<u8>() as u32;
-                self.pixels[offset + 0] = rand_val;
-                self.pixels[offset + 1] = rand_val;
-                self.pixels[offset + 2] = rand_val;
+                let u = (x as f32) / ((IMAGE_WIDTH-1) as f32);
+                let v = (y as f32) / ((IMAGE_HEIGHT-1) as f32);
+                let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+                let col = self.ray_color(&r);
+
+                self.pixels[offset + 0] = (col.x * 255.0) as u32;
+                self.pixels[offset + 1] = (col.y * 255.0) as u32;
+                self.pixels[offset + 2] = (col.z * 255.0) as u32;
+
+                // let rand_val: u32 = rng.gen::<u8>() as u32;
+                // self.pixels[offset + 0] = rand_val;
+                // self.pixels[offset + 1] = rand_val;
+                // self.pixels[offset + 2] = rand_val;
             }
             println!("done!");
         }
@@ -93,6 +117,12 @@ impl RSRaytracer {
         let end_time = std::time::Instant::now();
         let delta_time = end_time.duration_since(start_time);
         println!("Ray trace complete in {:?}.", delta_time);
+    }
+
+    fn ray_color(&self, ray: &Ray) -> Vec3 {
+        let direction = ray.direction.normalized();
+        let t = 0.5 * (direction.y + 1.0);
+        (1.0-t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
     }
 
     pub fn save_as_ppm(&self) -> io::Result<()> {
