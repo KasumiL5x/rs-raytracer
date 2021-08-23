2d98c5d746d84d597b20277e70a90b8d8d7b1249
KasumiL5x <KasumiL5x@gmail.com>
2021-08-24 00:03:04 +0100

    Formatted math.rs

--- src/math.rs
+++ src/math.rs
@@ -5,236 +5,237 @@ use std::ops;
 // --------------------------------------------------
 #[derive(Clone, Copy, Debug)]
 pub struct Vec3 {
-  pub x: f32,
-  pub y: f32,
-  pub z: f32
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vec3 {
-  pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
-    Vec3{x: x, y: y, z: z}
-  }
-
-  pub fn zero() -> Vec3 {
-    Vec3{x: 0.0, y: 0.0, z: 0.0}
-  }
-
-  pub fn length(&self) -> f32 {
-    (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
-  }
-
-  pub fn sqr_length(&self) -> f32 {
-    (self.x * self.x) + (self.y * self.y) + (self.z * self.z)
-  }
-
-  pub fn dot(&self, rhs: &Vec3) -> f32 {
-      (self.x * rhs.x) + (self.y * rhs.y) + (self.z * rhs.z)
-  }
-
-  pub fn cross(&self, rhs: &Vec3) -> Vec3 {
-    Vec3 {
-      x: self.y * rhs.z - self.z * rhs.y,
-      y: self.z * rhs.x - self.x * rhs.z,
-      z: self.x * rhs.y - self.y * rhs.x
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x: x, y: y, z: z }
     }
-  }
 
-  pub fn normalize(&mut self) -> Vec3 {
-    let len = self.length();
-    self.x /= len;
-    self.y /= len;
-    self.z /= len;
-    *self
-  }
-
-  pub fn normalized(&self) -> Vec3 {
-    let len = self.length();
-    Vec3 {
-      x: self.x / len,
-      y: self.y / len,
-      z: self.z / len
+    pub fn zero() -> Vec3 {
+        Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
     }
-  }
+
+    pub fn length(&self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    pub fn sqr_length(&self) -> f32 {
+        (self.x * self.x) + (self.y * self.y) + (self.z * self.z)
+    }
+
+    pub fn dot(&self, rhs: &Vec3) -> f32 {
+        (self.x * rhs.x) + (self.y * rhs.y) + (self.z * rhs.z)
+    }
+
+    pub fn cross(&self, rhs: &Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y * rhs.z - self.z * rhs.y,
+            y: self.z * rhs.x - self.x * rhs.z,
+            z: self.x * rhs.y - self.y * rhs.x,
+        }
+    }
+
+    pub fn normalize(&mut self) -> Vec3 {
+        let len = self.length();
+        self.x /= len;
+        self.y /= len;
+        self.z /= len;
+        *self
+    }
+
+    pub fn normalized(&self) -> Vec3 {
+        let len = self.length();
+        Vec3 {
+            x: self.x / len,
+            y: self.y / len,
+            z: self.z / len,
+        }
+    }
 }
 
 // Vec3 + Vec3
 impl ops::Add for Vec3 {
-  type Output = Vec3;
-  fn add(self, rhs: Vec3) -> Vec3 {
-    Vec3 {
-      x: self.x + rhs.x,
-      y: self.y + rhs.y,
-      z: self.z + rhs.z
+    type Output = Vec3;
+    fn add(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
     }
-  }
 }
 
 // Vec3 - Vec3
 impl ops::Sub for Vec3 {
-  type Output = Vec3;
-  fn sub(self, rhs: Vec3) -> Vec3 {
-    Vec3 {
-      x: self.x - rhs.x,
-      y: self.y - rhs.y,
-      z: self.z - rhs.z
+    type Output = Vec3;
+    fn sub(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
     }
-  }
 }
 
 // Vec3 * Vec3
 impl ops::Mul for Vec3 {
-  type Output = Vec3;
-  fn mul(self, rhs: Vec3) -> Vec3 {
-    Vec3 {
-      x: self.x * rhs.x,
-      y: self.y * rhs.y,
-      z: self.z * rhs.z
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
     }
-  }
 }
 
 // Vec3 * f32
 impl ops::Mul<f32> for Vec3 {
-  type Output = Vec3;
-  fn mul(self, rhs: f32) -> Vec3 {
-    Vec3 {
-      x: self.x * rhs,
-      y: self.y * rhs,
-      z: self.z * rhs
+    type Output = Vec3;
+    fn mul(self, rhs: f32) -> Vec3 {
+        Vec3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
     }
-  }
 }
 
 // f32 * Vec3
 impl ops::Mul<Vec3> for f32 {
-  type Output = Vec3;
-  fn mul(self, rhs: Vec3) -> Vec3 {
-    Vec3 {
-      x: self * rhs.x,
-      y: self * rhs.y,
-      z: self * rhs.z
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
     }
-  }
 }
 
 // Vec3 / f32
 impl ops::Div<f32> for Vec3 {
-  type Output = Vec3;
-  fn div(self, rhs: f32) -> Vec3 {
-    Vec3 {
-      x: self.x / rhs,
-      y: self.y / rhs,
-      z: self.z / rhs
+    type Output = Vec3;
+    fn div(self, rhs: f32) -> Vec3 {
+        Vec3 {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
     }
-  }
 }
 
 // -Vec3
 impl ops::Neg for Vec3 {
-  type Output = Vec3;
-  fn neg(self) -> Vec3 {
-    Vec3 {
-      x: -self.x,
-      y: -self.y,
-      z: -self.z
+    type Output = Vec3;
+    fn neg(self) -> Vec3 {
+        Vec3 {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
     }
-  }
 }
-
-
 
 // --------------------------------------------------
 // Ray
 // --------------------------------------------------
 #[derive(Clone, Copy, Debug)]
 pub struct Ray {
-  pub origin: Vec3,
-  pub direction: Vec3
+    pub origin: Vec3,
+    pub direction: Vec3,
 }
 
 impl Ray {
-  pub fn new(origin: Vec3, direction: Vec3) -> Ray {
-    Ray {
-      origin: origin,
-      direction: direction
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+        Ray {
+            origin: origin,
+            direction: direction,
+        }
     }
-  }
 
-  pub fn at(&self, t: f32) -> Vec3 {
-    self.origin + self.direction * t
-  }
+    pub fn at(&self, t: f32) -> Vec3 {
+        self.origin + self.direction * t
+    }
 }
 
 // --------------------------------------------------
 // Hittable / Geometric Objects
 // --------------------------------------------------
 pub struct HitRecord {
-  pub p: Vec3,
-  pub n: Vec3,
-  pub t: f32
+    pub p: Vec3,
+    pub n: Vec3,
+    pub t: f32,
 }
 trait Hittable {
-  fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, out_hit: &mut HitRecord) -> bool;
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, out_hit: &mut HitRecord) -> bool;
 }
 
 pub struct Sphere {
-  pub center: Vec3,
-  pub radius: f32
+    pub center: Vec3,
+    pub radius: f32,
 }
 
 impl Sphere {
-  pub fn new(center: Vec3, radius: f32) -> Sphere {
-    Sphere {
-      center: center,
-      radius: radius
+    pub fn new(center: Vec3, radius: f32) -> Sphere {
+        Sphere {
+            center: center,
+            radius: radius,
+        }
     }
-  }
 }
 
 impl Hittable for Sphere {
-  fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, out_hit: &mut HitRecord) -> bool {
-    let oc = ray.origin - self.center;
-    let a = ray.direction.sqr_length();
-    let half_b = oc.dot(&ray.direction);
-    let c = oc.sqr_length() - (self.radius * self.radius);
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32, out_hit: &mut HitRecord) -> bool {
+        let oc = ray.origin - self.center;
+        let a = ray.direction.sqr_length();
+        let half_b = oc.dot(&ray.direction);
+        let c = oc.sqr_length() - (self.radius * self.radius);
 
-    let discriminant = (half_b * half_b) - (a * c);
-    if discriminant < 0.0 {
-      return false
+        let discriminant = (half_b * half_b) - (a * c);
+        if discriminant < 0.0 {
+            return false;
+        }
+        let sqrtd = discriminant.sqrt();
+
+        // Find the nearest root that lies in the acceptable range.
+        let mut root = (-half_b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return false;
+            }
+        }
+
+        out_hit.t = root;
+        out_hit.p = ray.at(out_hit.t);
+        out_hit.n = (out_hit.p - self.center) / self.radius;
+
+        return true;
     }
-    let sqrtd = discriminant.sqrt();
-
-    // Find the nearest root that lies in the acceptable range.
-    let mut root = (-half_b - sqrtd) / a;
-    if root < t_min || t_max < root {
-      root = (-half_b + sqrtd) / a;
-      if root < t_min || t_max < root {
-        return false
-      }
-    }
-
-    out_hit.t = root;
-    out_hit.p = ray.at(out_hit.t);
-    out_hit.n = (out_hit.p - self.center) / self.radius;
-
-    return true
-  }
 }
-
 
 // --------------------------------------------------
 // Intersections
 // --------------------------------------------------
 pub fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> f32 {
-  let oc = ray.origin - (*center);
-  let a = ray.direction.sqr_length();
-  let half_b = oc.dot(&ray.direction);
-  let c = oc.sqr_length() - (radius * radius);
-  let discriminant = (half_b * half_b) - (a * c);
+    let oc = ray.origin - (*center);
+    let a = ray.direction.sqr_length();
+    let half_b = oc.dot(&ray.direction);
+    let c = oc.sqr_length() - (radius * radius);
+    let discriminant = (half_b * half_b) - (a * c);
 
-  if discriminant < 0.0 {
-    return -1.0
-  } else {
-    return (-half_b - discriminant.sqrt()) / a
-  }
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-half_b - discriminant.sqrt()) / a;
+    }
 }
